c7d4143df57af519632fc6a2b8e47584d1de059c
osvaldoarriagada <osvaldo.arriagada@gmail.com>
2023-04-24 16:47:47 -0400

    frist commit

---
+++ variables_tipos.py
@@ -0,0 +1,34 @@
+## Asignar un número entero a una variable llamada edad y mostrarlo en pantalla.
+edad = 34
+print(edad)
+
+#Asignar una cadena de texto a una variable llamada nombre y mostrarla en pantalla.
+Nombre = "pedro"
+print(Nombre)
+
+#Asignar dos números a las variables a y b, y luego intercambiar sus valores sin utilizar una tercera variable.
+
+a = 1 
+b = 2 
+
+a,b = b,a 
+print(a)
+
+#Calcular la suma de dos números y asignar el resultado a una variable llamada resultado, luego mostrar el resultado en pantalla.
+
+
+suma = a + b 
+print(suma)
+
+#Asignar una lista de nombres a una variable llamada nombres y mostrar el primer nombre de la lista.
+nombres =["juan", "pedro","pablo"]
+primer_nombre = nombres[-1]
+print(primer_nombre)
+
+
+
+
+
+
+
+
